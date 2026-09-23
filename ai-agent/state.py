eb852a9ce55141ed4a6ca3415c48731dbe9e0c56
sakshamagrawal9170680802-spec <sakshamagrawal9170680802@gmail.com>
2026-09-23 20:16:46 +0530

    Delete ai-agent directory

--- ai-agent/state.py
+++
@@ -1,5 +0,0 @@
-from typing import TypedDict,Annotated
-from langgraph.graph.message import add_messages
-
-class Aegis_State(TypedDict):
-    messages:Annotated[list,add_messages]# here add_messsage is a reducer function that tells what to do woth new value going to enter list whether to append it or write it
